421a708ba8272cda708391a325a54caaa44e2e67
Joe Stanton <joe@joestanton.co.uk>
2011-03-15 00:34:37 +0000

    Project

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,8 +6,7 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Test");
-		System.out.println("Test 2");
+	
 	}
 
 }
