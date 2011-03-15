7a515ecb2fa36c824025781b5cee3fa075f838a2
Joe Stanton <joe@joestanton.co.uk>
2011-03-15 01:01:56 +0000

    Joe's First Change

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Marcus' First Change");
+		System.out.println("Joe's First Change");
 	}
 
 }
